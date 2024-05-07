ee0ef86ed10dd4392ff853459392756345db80e2
Juan Carlos <juancarloslacruzlacruz@outlook.com>
2024-05-07 09:02:37 +0200

    authorization header added

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ app.use((req, res, next) => {
 	res.setHeader('Access-Control-Allow-Origin', '*');
 	res.setHeader('Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE');
 	res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+	res.setHeader('Access-Control-Allow-Headers', 'authorization');
 	next();
 });
 
